27b62f24a81b2ffd250c872f2bb2a2d098849476
Kurtis Mulgrew <kmulgrew1490@hotmail.com>
2020-02-14 21:57:09 +0000

    db

---
+++ Database/MovieContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Database
+{
+    public partial class MovieContext : DbContext
+    {
+        public MovieContext()
+        {
+        }
+
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movies> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movies>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Endyear).HasColumnName("endyear");
+
+                entity.Property(e => e.Genres)
+                    .HasColumnName("genres")
+                    .HasMaxLength(1000)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Isadult).HasColumnName("isadult");
+
+                entity.Property(e => e.Originaltitle)
+                    .HasColumnName("originaltitle")
+                    .HasMaxLength(1000)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Primarytitle)
+                    .HasColumnName("primarytitle")
+                    .HasMaxLength(1000)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Runtimeminutes).HasColumnName("runtimeminutes");
+
+                entity.Property(e => e.Startyear).HasColumnName("startyear");
+
+                entity.Property(e => e.Tconst)
+                    .HasColumnName("tconst")
+                    .HasMaxLength(100)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Titletype)
+                    .HasColumnName("titletype")
+                    .HasMaxLength(1000)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Database/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database
+{
+    public partial class Movies
+    {
+        public string Tconst { get; set; }
+        public string Titletype { get; set; }
+        public string Primarytitle { get; set; }
+        public string Originaltitle { get; set; }
+        public bool? Isadult { get; set; }
+        public int? Startyear { get; set; }
+        public int? Endyear { get; set; }
+        public int? Runtimeminutes { get; set; }
+        public string Genres { get; set; }
+    }
+}
