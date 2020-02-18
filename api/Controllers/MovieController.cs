93b7f5f1dac20460163170d2366a10b6837e5b05
Kurtis Mulgrew <kmulgrew1490@hotmail.com>
2020-02-18 23:07:22 +0000

    redis implementation

--- api/Controllers/MovieController.cs
+++ api/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using api.Interfaces;
 using Database;
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis.Extensions.Core.Abstractions;
 using System.Collections.Generic;
 
 namespace api.Controllers
@@ -10,14 +11,19 @@ namespace api.Controllers
     public class MovieController : ControllerBase
     {
         private readonly IGetMovie getMovie;
+        private readonly IRedisCacheClient redis;
 
-        public MovieController(IGetMovie getMovie)
+        public MovieController(IGetMovie getMovie, IRedisCacheClient redis)
         {
             this.getMovie = getMovie;
+            this.redis = redis;
         }
 
         [HttpGet("{title}")]
-        public IEnumerable<Movies> Get(string title) => this.getMovie.GetMovies(title);
-        
+        public IEnumerable<Movies> Get(string title) { 
+
+            return this.getMovie.GetMovies(title); 
+        }
+
     }
 }
--- api/Startup.cs
+++ api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis.Extensions.Core.Configuration;
 
 namespace api
 {
@@ -22,6 +23,9 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var redisConfig = Configuration.GetSection("Redis").Get<RedisConfiguration>();
+            services.AddSingleton(redisConfig);
+
             services.AddScoped<IGetMovie, GetMovies>();
             services.AddDbContext<MovieContext>(options =>
             options.UseNpgsql(Configuration.GetConnectionString("MoviesDb")));
